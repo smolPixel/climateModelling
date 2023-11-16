5dc84db5e91d98e3735bb5c1c56f39e8c8a5fd38
frederic <bemeraude@hotmail.com>
2023-11-16 14:16:57 -0500

    auto commit, minor modification

--- Tuto.py
+++ Tuto.py
@@ -1,9 +1,12 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df=pd.read_csv('data/ForestFiresUCIIrvine/forestfires.csv')
 df['day'] = ((df['day'] == 'sun') | (df['day'] == 'sat')| (df['day'] == 'frid'))
-print(df['day'])
 df=df.rename(columns={'day': 'is_weekend'})
-print(df)
-
+features = df.drop(['area'], axis = 1)
+labels = df['area'].values.reshape(-1, 1)
 #Some encoding: We'll change the months to 1-12 and the days to 0/1 for weekdays/weekend (friday included)
+X_train, X_test, y_train, y_test = train_test_split(features,labels, test_size = 0.2, random_state = 42)
+
+print(X_train, y_train)
