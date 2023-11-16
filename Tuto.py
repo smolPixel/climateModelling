abbc2cf0271475ff6e749439df9758a2eae25671
frederic <bemeraude@hotmail.com>
2023-11-16 14:21:49 -0500

    auto commit, minor modification

--- Tuto.py
+++ Tuto.py
@@ -10,6 +10,6 @@ labels = df['area'].values.reshape(-1, 1)
 #Some encoding: We'll change the months to 1-12 and the days to 0/1 for weekdays/weekend (friday included)
 X_train, X_test, y_train, y_test = train_test_split(features,labels, test_size = 0.2, random_state = 42)
 
-print(X_train, y_train)
+print(X_train)
 
 clf=LogisticRegression().fit(X_train, y_train)
