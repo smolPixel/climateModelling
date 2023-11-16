6486ca8e8718a8dbca39d1abc9e48c6db24b36d9
frederic <bemeraude@hotmail.com>
2023-11-16 14:12:21 -0500

    auto commit, minor modification

--- Tuto.py
+++ Tuto.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
 df=pd.read_csv('data/ForestFiresUCIIrvine/forestfires.csv')
+df['day'] = ((df['day'] == 'sun') | (df['day'] == 'sat')| (df['day'] == 'frid'))
+print(df['day'])
+df=df.rename(columns={'day': 'is_weekend'})
 print(df)
+
+#Some encoding: We'll change the months to 1-12 and the days to 0/1 for weekdays/weekend (friday included)
