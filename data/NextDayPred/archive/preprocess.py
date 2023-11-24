90986f01161fde66f29315a480668b73525946b5
frederic <bemeraude@hotmail.com>
2023-11-24 15:42:35 -0500

    auto commit, minor modification

--- data/NextDayPred/archive/preprocess.py
+++ data/NextDayPred/archive/preprocess.py
@@ -1,4 +1,7 @@
 from torchdata.datapipes.iter import FileLister, FileOpener
+import json
+
+set='train'
 
 datapipe1 = FileLister(".", "*.tfrecord")
 datapipe2 = FileOpener(datapipe1, mode="b")
@@ -8,6 +11,9 @@ print('hello')
 i=0
 for example in tfrecord_loader_dp:
 	i+=1
+	jj=json.dumps(example)
+	print(jj)
+	fds
 	# print(example)
 	# fds
 print(i)
