a8619ae68700af920b93e2f6af691c3ab969ab31
frederic <bemeraude@hotmail.com>
2023-11-25 09:42:23 -0500

    auto commit, minor modification

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from utils import Envirodataset
+import yaml
+import argparse
 
 train=Envirodataset('data/NextDayPred/archive/train.jsonl')
 dev=Envirodataset('data/NextDayPred/archive/dev.jsonl')
 test=Envirodataset('data/NextDayPred/archive/test.jsonl')
 
 print(len(train), len(dev), len(test))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='VAE for data augmentation')
+    parser.add_argument('--config_file', type=str)
+    args = parser.parse_args()
+    args = args.__dict__
+
+    stream = open(args['config_file'], "r")
+    argdict = yaml.safe_load(stream)
+    print(argdict)
+    fds
